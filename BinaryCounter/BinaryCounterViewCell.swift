10cc8fa981613b769e5a1abfd1f9d085e6f58d46
Admin <admin@Admins-MacBook-Pro.local>
2017-11-27 14:02:16 -0800

    Binary Counter App

---
+++ BinaryCounter/BinaryCounterViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BinaryCounterViewCell.swift
+//  BinaryCounter
+//
+//  Created by Admin on 11/9/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import UIKit
+
+class BinaryCounterViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
